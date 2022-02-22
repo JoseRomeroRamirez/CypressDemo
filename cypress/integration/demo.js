d32fc4f5c1e4ccf37dce8564b25218b4e0b1c364
JoseRomeroRamirez <77643339+JoseRomeroRamirez@users.noreply.github.com>
2022-02-22 00:33:17 -0600

    first commit

---
+++ cypress/integration/demo.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+it('login test', function(){
+    cy.visit('/')
+    cy.get('[data-test="username"]').type("standard_user")
+    cy.get('[data-test="password"]').type("secret_sauce")
+    cy.get('[data-test="login-button"]').click()
+    cy.url().should('include', '/inventory.html')
+    cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    cy.get('.shopping_cart_link').click()
+    cy.url().should('include', '/cart.html')
+    cy.get('.shopping_cart_badge').should('contain', '1')
+})
+it
