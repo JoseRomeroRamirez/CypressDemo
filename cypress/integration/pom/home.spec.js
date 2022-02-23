598d28bda73b827ac90658556a80f094e2e8d4cf
JoseRomeroRamirez <77643339+JoseRomeroRamirez@users.noreply.github.com>
2022-02-23 01:29:10 -0600

    Comments added

--- cypress/integration/pom/home.spec.js
+++ cypress/integration/pom/home.spec.js
@@ -4,17 +4,17 @@ import inventoryPage from "../../pages/swaglabs/inventoryPage";
 const tests = require('../../fixtures/dataDriven/swagLabsUsers.json')
 
 describe('DEMO POM', () =>{
-
+    //Before each test
     beforeEach(()=>{
         cy.visit('/') 
     });
-
+    //Loop using data provider
     tests.forEach(test => {
         it(test.name , () =>{
             homePage.typeUser(test.user);
             homePage.typePass(test.pass);
             homePage.clickLogin();
-
+            //Validate the expected text match the actual text
             if(test.name == 'should login to inventory page'){
                 inventoryPage.elements.titleSpan().should('have.text', test.expected)
             }else{
--- cypress/pages/swaglabs/homePage.js
+++ cypress/pages/swaglabs/homePage.js
@@ -5,15 +5,15 @@ class homePage{
         loginButton: () => cy.get('#login-button'),
         errorMessage: () => cy.get('h3[data-test="error"]')
     }
-
+    //This method type the user in the user input text box
     typeUser(user){
         this.elements.userInput().type(user);
     }
-
+    //This method type the pass in the user input text box
     typePass(pass){
         this.elements.passInput().type(pass);
     }
-
+    //This method click the login button
     clickLogin(){
         this.elements.loginButton().click();
     }
